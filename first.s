b26bb3e0b5e8975cb93d17921343da45710c8bf5
Diaconescu Rares Andrei Ciprian <34660424+DiaRar@users.noreply.github.com>
2023-09-06 16:53:41 +0000

    Update First

--- first.s
+++ first.s
@@ -1,1 +1,21 @@
+.data
+.globl hello
+hello:
+.string "Hello World!\n"
 
+.global main
+
+main:
+        pushq   %rbp
+        movq    %rsp, %rbp
+        
+        movq    $0, %rax
+        movq    $hello, %rdi
+        call    puts
+        
+        movq    %rbp, %rsp
+        popq    %rbp
+end:
+        movq    $0, %rsp
+        call    exit
+        
